1f351a4e5c577a8eeb047e98efbd26f6907a5278
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-13 10:18:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,45 @@ const burmeseMap = {
 
 const thaiMap = {
     'a': 'ก', 'b': 'ข', 'c': 'จ', 'd': 'ด', 'e': 'เ', 'f': 'ฟ', 'g': 'ก',
-    'h':
+    'h': 'ห', 'i': 'ิ', 'j': 'จ', 'k': 'ค', 'l': 'ล', 'm': 'ม', 'n': 'น',
+    'o': 'โ', 'p': 'ป', 'q': 'ค', 'r': 'ร', 's': 'ส', 't': 'ต', 'u': 'ุ',
+    'v': 'ว', 'w': 'ว', 'x': 'ซ', 'y': 'ย', 'z': 'ซ', ' ': ' ', ',': ',', '.': '.'
+};
+
+const persianMap = {
+    'a': 'ا', 'b': 'ب', 'c': 'ج', 'd': 'د', 'e': 'ه', 'f': 'ف', 'g': 'گ',
+    'h': 'ه', 'i': 'ی', 'j': 'ج', 'k': 'ک', 'l': 'ل', 'm': 'م', 'n': 'ن',
+    'o': 'و', 'p': 'پ', 'q': 'ق', 'r': 'ر', 's': 'س', 't': 'ت', 'u': 'و',
+    'v': 'و', 'w': 'و', 'x': 'خ', 'y': 'ی', 'z': 'ز', ' ': ' ', ',': ',', '.': '.'
+};
+
+function convertText(script) {
+    const inputText = document.getElementById('inputText').value;
+    let outputText = '';
+
+    switch(script) {
+        case 'armenian':
+            outputText = translateText(inputText, armenianMap);
+            break;
+        case 'georgian':
+            outputText = translateText(inputText, georgianMap);
+            break;
+        case 'burmese':
+            outputText = translateText(inputText, burmeseMap);
+            break;
+        case 'thai':
+            outputText = translateText(inputText, thaiMap);
+            break;
+        case 'persian':
+            outputText = translateText(inputText, persianMap);
+            break;
+        default:
+            outputText = inputText;
+    }
+
+    document.getElementById('outputText').value = outputText;
+}
+
+function translateText(text, map) {
+    return text.split('').map(char => map[char.toLowerCase()] || char).join('');
+}
